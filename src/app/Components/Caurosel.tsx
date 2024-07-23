45a75865f7d6945f363d0398febaa2868d9901c8
Vinicius Fernandes <vrfern0ndes@gmail.com>
2024-07-23 19:48:54 -0400

    update

--- src/app/Components/Caurosel.tsx
+++ src/app/Components/Caurosel.tsx
@@ -1,4 +1,4 @@
-'use client'
+'use client';
 
 import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
@@ -9,24 +9,24 @@ import { Autoplay, Navigation, Pagination } from 'swiper/modules';
 
 const Carousel = () => {
   const slides = [
-    { src: './item1.png', title: 'Toalha de banho', price: '80x140 cm' },
-    { src: './item2.png', title: 'Toalha de rosto', price: '43x66 cm' },
-    { src: './item3.png', title: 'Tapete POP', price: '40x50 cm' },
-    { src: './item5-.png', title: 'Pano de chão', price: '58x88 cm' },
-    { src: './item6.png', title: 'Pano de chão flanelado', price: '42x66 cm' },
-    { src: './item7.png', title: 'Pano de prato', price: '45x70 cm' },
-    { src: './item8.png', title: 'Atoalhado', price: '40x60 cm' },
-    { src: './item9.png', title: 'Tabua Slim Pop', price: '28x13 cm' },
-    { src: './item10.png', title: 'Pano MICROFIBRA', price: '30x30 cm' },
-    { src: './item11.png', title: 'Esponja Poliester Multiuso', price: '07x11 cm' },
-    { src: './item12.png', title: 'Escova Fácil Alcance', price: '29x7 cm' },
+    { src: '/item1.png', title: 'Toalha de banho', price: '80x140 cm' },
+    { src: '/item2.png', title: 'Toalha de rosto', price: '43x66 cm' },
+    { src: '/item3.png', title: 'Tapete POP', price: '40x50 cm' },
+    { src: '/item5-.png', title: 'Pano de chão', price: '58x88 cm' },
+    { src: '/item6.png', title: 'Pano de chão flanelado', price: '42x66 cm' },
+    { src: '/item7.png', title: 'Pano de prato', price: '45x70 cm' },
+    { src: '/item8.png', title: 'Atoalhado', price: '40x60 cm' },
+    { src: '/item9.png', title: 'Tabua Slim Pop', price: '28x13 cm' },
+    { src: '/item10.png', title: 'Pano MICROFIBRA', price: '30x30 cm' },
+    { src: '/item11.png', title: 'Esponja Poliester Multiuso', price: '07x11 cm' },
+    { src: '/item12.png', title: 'Escova Fácil Alcance', price: '29x7 cm' },
   ];
 
   return (
-    <main>
-      <section className="mx-auto mr-9 grid max-w-screen-2xl gap-4 mt-6 px-1 pb-2 md:grid-cols-7 md:grid-rows-2">
+    <main className="px-4 sm:px-6 md:px-8 lg:px-10 xl:px-12">
+      <section className="mx-auto grid max-w-screen-2xl gap-4 mt-6 pb-2 md:grid-cols-7 md:grid-rows-2 sm:px-4 md:px-6">
         <div className="md:col-span-4 md:row-span-2">
-          <a className="relative block aspect-square h-full w-full" href="">
+          <a className="relative block aspect-square h-full w-full" href="#">
             <div className="group flex h-full w-full items-center justify-center overflow-hidden rounded-lg border bg-white hover:border-blue-500 dark:bg-white relative border-neutral-200 dark:border-neutral-100">
               <img className="w-[1000px] inset-2 text-transparent" src={slides[0].src} alt={slides[0].title} />
             </div>
@@ -44,10 +44,10 @@ const Carousel = () => {
         </div>
         {slides.slice(1, 3).map((slide, index) => (
           <div className="md:col-span-2 md:row-span-1" key={index}>
-            <a className="relative block aspect-square h-full w-full" href="">
+            <a className="relative block aspect-square h-full w-full" href="#">
               <div className="group flex h-full w-full items-center justify-center overflow-hidden rounded-lg border bg-white hover:border-blue-500 dark:bg-white relative border-neutral-200 dark:border-neutral-100">
                 <img className="h-full w-full inset-0 text-transparent" src={slide.src} alt={slide.title} />
-                <div className="absolute bottom-0 left-0 flex w-full px-4 pb-4 @container/label">
+                <div className="absolute bottom-0 left-0 flex w-full px-4 pb-4">
                   <div className="flex items-center rounded-full border bg-white/70 p-1 text-xs font-semibold text-black backdrop-blur-md dark:border-neutral-800 dark:bg-black/70 dark:text-white">
                     <h3 className="mr-4 line-clamp-2 flex-grow pl-2 leading-none tracking-tight">
                       {slide.title}
@@ -63,10 +63,10 @@ const Carousel = () => {
         ))}
       </section>
 
-      <div className="w-full overflow-x-auto pb-6 pt-1 p-8">
+      <div className="w-full overflow-x-auto pb-6 pt-1">
         <Swiper
           spaceBetween={20}
-          slidesPerView={4} // Ajuste para mostrar 4 imagens por padrão
+          slidesPerView={1}
           loop={true}
           speed={3900}
           autoplay={{
@@ -81,10 +81,10 @@ const Carousel = () => {
               slidesPerView: 2,
             },
             900: {
-              slidesPerView: 3, // Para telas menores que 900px, mostrar 3 imagens
+              slidesPerView: 3,
             },
             1024: {
-              slidesPerView: 4, // Para telas maiores que 1024px, mostrar 4 imagens
+              slidesPerView: 4,
             },
           }}
           modules={[Autoplay, Navigation, Pagination]}
@@ -92,15 +92,15 @@ const Carousel = () => {
           {slides.map((slide, index) => (
             <SwiperSlide key={index}>
               <div className="relative aspect-square h-[30vh] max-h-[275px] w-full flex-none">
-                <a className="relative h-full w-full" href=""></a>
+                <a className="relative h-full w-full" href="#"></a>
                 <div className="group flex h-full w-full items-center justify-center overflow-hidden rounded-lg border bg-white hover:border-blue-600 dark:bg-white relative border-neutral-200 dark:border-neutral-100">
                   <img
                     className="relative h-full w-full object-contain transition duration-300 ease-in-out group-hover:scale-105"
                     src={slide.src}
-                    alt=""
+                    alt={slide.title}
                   />
                 </div>
-                <div className='absolute bottom-0 left-0 flex w-full px-4 pb-4 @container/label'>
+                <div className='absolute bottom-0 left-0 flex w-full px-4 pb-4'>
                   <div className='flex items-center rounded-full border bg-white/70 p-1 text-xs font-semibold text-black backdrop-blur-md dark:border-neutral-800 dark:bg-black/70 dark:text-white'>
                     <h3 className='mr-4 line-clamp-2 flex-grow pl-2 leading-none tracking-tight'>
                       {slide.title}
--- src/app/Components/Footer.tsx
+++ src/app/Components/Footer.tsx
@@ -9,62 +9,43 @@ const Footer = () => {
     <div className='flex w-full py-8 items-start max-md:items-center justify-evenly bg-white contrast:bg-gray-950 max-md:flex-col max-md:gap-6'>
       
       <div className='flex flex-col items-start gap-4'>
-        <div className='flex flex-col items-center gap-1 justify-center'>
+        <div className='flex flex-col items-center gap-3 justify-center'>
           <div className='mr-2'>
-            <img src='./Logo.png' alt='' className='max-w-[400px]' />
+            <img src='./Logo.png' alt='' className='max-w-[600px]' />
           </div>
           <div className='flex flex-col'>
-            <p className=''>2024 TR COMERCIAL - TODOS OS DIREITOS RESERVADOS</p>
+            <p className='text-sm'>2024 TR COMERCIAL - TODOS OS DIREITOS RESERVADOS</p>
           </div>
         </div>
       </div>
       <div className='flex flex-col items-start gap-4 max-md:gap-8'> 
         <div className='w-full inline-flex items-center text-[#293A69] contrast:text-white font-semibold text-3xl max-md:text-base'>    
             <img className='w-6 mr-3 mmd:w-8 mmd:mr-6' src="/Polygon8.svg" alt="" />
+            <div className='inline-flex bg-[#293A69] rounded-md p-2 mr-5 min-w-[206px] justify-center text-white text-sm'> 
+              <a href="https://www.instagram.com/trcomercial.manaus/"> 
+              TRCOMERCIAL
+              </a>
+            </div>
             Siga Nossa Rede Social
         </div>
         <div className='flex gap-4 max-md:flex-col w-full items-center'>   
             <div className='flex flex-col justify-start '>   
                 <a className='flex flex-col items-center text-white font-medium text-sm gap-4 max-md:text-xs' href="https://www.instagram.com/trcomercial.manaus/">    
-                  <div className='inline-flex bg-[#293A69] rounded-md p-2 gap-2 min-w-[206px] justify-center'> 
-                    <svg className='color-white' xmlns='http://www.w3.org/2000/svg' fill='white' width={16} height={16} viewBox='0 0 24 24'>   
-                        <path d='M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z'></path>
-                    </svg>
-                    "Trcomercial"
-                  </div>
                   <img className='max-w-[250px] max-md:max-w-[200px] object-fill max-md:hidden' src="./instagram1.jpeg" alt="" />
                 </a>
             </div>
             <div className='flex flex-col justify-start '>   
                 <a className='flex flex-col items-center text-white font-medium text-sm gap-4 max-md:text-xs' href="https://www.instagram.com/trcomercial.manaus/">    
-                  <div className='inline-flex bg-[#293A69] rounded-md p-2 gap-2 min-w-[206px] justify-center'> 
-                    <svg className='color-white' xmlns='http://www.w3.org/2000/svg' fill='white' width={16} height={16} viewBox='0 0 24 24'>   
-                        <path d='M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z'></path>
-                    </svg>
-                    "Trcomercial"
-                  </div>
                   <img className='max-w-[250px] max-md:max-w-[200px] object-fill max-md:hidden' src="./instagram2.jpeg" alt="" />
                 </a>
             </div>
             <div className='flex flex-col justify-start '>   
                 <a className='flex flex-col items-center text-white font-medium text-sm gap-4 max-md:text-xs' href="https://www.instagram.com/trcomercial.manaus/">    
-                  <div className='inline-flex bg-[#293A69] rounded-md p-2 gap-2 min-w-[206px] justify-center'> 
-                    <svg className='color-white' xmlns='http://www.w3.org/2000/svg' fill='white' width={16} height={16} viewBox='0 0 24 24'>   
-                        <path d='M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z'></path>
-                    </svg>
-                    "Trcomercial"
-                  </div>
                   <img className='max-w-[250px] max-md:max-w-[200px] object-fill max-md:hidden' src="./instagram3.jpeg" alt="" />
                 </a>
             </div>
             <div className='flex flex-col justify-start '>   
                 <a className='flex flex-col items-center text-white font-medium text-sm gap-4 max-md:text-xs' href="https://www.instagram.com/trcomercial.manaus/">    
-                  <div className='inline-flex bg-[#293A69] rounded-md p-2 gap-2 min-w-[206px] justify-center'> 
-                    <svg className='color-white' xmlns='http://www.w3.org/2000/svg' fill='white' width={16} height={16} viewBox='0 0 24 24'>   
-                        <path d='M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z'></path>
-                    </svg>
-                    "Trcomercial"
-                  </div>
                   <img className='max-w-[250px] max-md:max-w-[200px] object-fill max-md:hidden' src="./instagram3.jpeg" alt="" />
                 </a>
             </div>
